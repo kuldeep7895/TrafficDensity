e4e3f24933fba1b208c34fa8acacf4209856d6ac
kuldeep7895 <kuldeepsan09@gmail.com>
2021-02-20 17:29:01 +0530

    Code for mouse press and coordinate detection

---
+++ project.cpp
@@ -0,0 +1,38 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include "opencv2/calib3d/calib3d.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+vector<Point2f> manualPoints;
+vector<Point2f> destPoints;
+Mat originalImg;
+Mat resImg;
+static void onMouse( int event, int x, int y, int, void* ){
+	if  ( event == EVENT_LBUTTONDOWN ){
+		if(manualPoints.size()<4){
+//			circle(originalImg,Point2f(x, y),320,Scalar( 0, 55, 255 ),FILLED,LINE_8 );
+			circle(originalImg,Point(x,y),10,Scalar(0,69,255),FILLED);
+			manualPoints.push_back(Point2f(x, y));
+		}else{
+			cout<<"You have already selected forur points required"<<endl;
+		}
+		cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	}
+
+}
+
+int main(){
+	string path = "Resources/empty.jpg"; 
+	originalImg = imread(path,1);
+	cvtColor(originalImg,originalImg,COLOR_BGR2GRAY);
+	Mat orgCopy = originalImg.clone();
+//	circle(originalImg,Point2f(100,200),320,Scalar( 0, 55, 255 ),FILLED,LINE_8 );
+	namedWindow("OriginalImage");
+	setMouseCallback( "OriginalImage", onMouse, 0 );
+	imshow("OriginalImage",originalImg);
+}
