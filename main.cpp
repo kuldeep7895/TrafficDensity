e211cdb7567115942a2732d0d21015abd31cd266
kuldeep7895 <kuldeepsan09@gmail.com>
2021-02-23 20:57:23 +0530

    Separate files as suggested and contributed by Aryan

--- main.cpp
+++ main.cpp
@@ -1,49 +1,13 @@
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/objdetect.hpp>
-#include "opencv2/calib3d/calib3d.hpp"
-#include <iostream>
-
-using namespace std;
-using namespace cv;
+#include "functions.h"
 
 vector<Point2f> manualPoints;
 vector<Point2f> destPoints;
 Mat originalImg;
 Mat resImg;
 
-static void onMouse( int event, int x, int y, int, void* )
-{
-	
-	if  ( event == EVENT_LBUTTONDOWN )
-	{
-		
-		if(manualPoints.size()<4)
-		{
-
-			circle(originalImg,Point(x,y),10,Scalar(0,69,255),FILLED);
-			manualPoints.push_back(Point2f(x, y));
-		
-		}
-		
-		else
-		{
-			
-			cout<<"You have already selected forur points required"<<endl;
-		
-		}
-		
-		cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
-	
-	}
-
-}
-
-
 int main(int argc, char **argv)
 {
-	
+
 	if(argc != 2)
 	{
 	
@@ -63,18 +27,18 @@ int main(int argc, char **argv)
 		return 2;
 	
 	}
-	 
-//	originalImg = imread(path,1);
 	
 	cvtColor(originalImg,originalImg,COLOR_BGR2GRAY);
+	
+	//resize(originalImg, originalImg, Size(originalImg.cols/2, originalImg.rows/2));
+	
 	Mat orgCopy = originalImg.clone();
 
-//	circle(originalImg,Point2f(100,200),320,Scalar( 0, 55, 255 ),FILLED,LINE_8 );
-	
 	namedWindow("Original Frame");
-	setMouseCallback( "Original Frame", onMouse, 0 );
-	
-	while(manualPoints.size()<4)		//manual point selection
+
+	setMouseCallback("Original Frame", onMouse, 0 );
+
+	while(manualPoints.size()<4)
 	{
 		
 		imshow("Original Frame",originalImg);
@@ -85,7 +49,7 @@ int main(int argc, char **argv)
 	imshow("Original Frame",originalImg);
 	
 	destroyWindow("Original Frame");
-	
+
 	Mat out;
 
 	destPoints.push_back(Point2f(472,52));
@@ -94,6 +58,7 @@ int main(int argc, char **argv)
 	destPoints.push_back(Point2f(472,830));
 
 	Mat homograph = findHomography(manualPoints,destPoints);
+	
 	warpPerspective(orgCopy,out,homograph,originalImg.size());
 	imshow("Projected Frame",out);
 	waitKey(0);
