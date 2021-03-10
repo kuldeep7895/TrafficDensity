fd9bee8fb8b24b5a885fac09500a6ff6030cc55d
Aryan10333 <aryanchandra2@gmail.com>
2021-03-10 13:10:53 +0530

    Added function for calculating dynamic density.

--- main.cpp
+++ main.cpp
@@ -34,6 +34,8 @@ int main(int argc, char **argv)
 	
 	cvtColor(originalImg,originalImg,COLOR_BGR2GRAY);
 	
+	//resize(originalImg, originalImg, Size(originalImg.cols/2, originalImg.rows/2));
+	
 	Mat orgCopy = originalImg.clone();
 
 	namedWindow("Original Frame");
@@ -63,7 +65,13 @@ int main(int argc, char **argv)
 	
 	warpPerspective(orgCopy,out,homograph,originalImg.size());
 	warpPerspective(orgCopy,out,homograph,Size(originalImg.size().width-400,originalImg.size().height));
-
+	//imshow("Projected Frame",out);
+	//waitKey(0);
+	
+	//imwrite("Projected_image.jpg", out);
+	
+	//destroyWindow("Projected Frame");
+	
 	Rect rect = boundingRect(destPoints);
 	
 	Mat crop_image = out(rect);
@@ -74,8 +82,93 @@ int main(int argc, char **argv)
 	imwrite("Cropped_image.jpg", crop_image);
 	destroyWindow("Cropped Frame");
 	
-    	VideoCapture cap("trafficvideo.mp4");
+	VideoCapture capture("trafficvideo.mp4");
+    	 
+    	if (!capture.isOpened())
+    	{
+        	//error in opening the video input
+        	cerr << "Unable to open file!" << endl;
+        
+        	return 0;
     	
+    	}
+
+    	Mat frame1, prvs;
+	capture >> frame1;
+	cvtColor(frame1, prvs, COLOR_BGR2GRAY);
+	
+	int count = 1;
+	
+	ofstream fout;  
+  
+	fout.open("dynamic_density.txt"); 
+
+	while(true)
+	{
+
+		Mat frame2, next;
+		capture >> frame2;
+		
+		if (frame2.empty())
+		    break;
+		
+		if(count % 5 == 0)
+		{
+		
+			cvtColor(frame2, next, COLOR_BGR2GRAY);
+
+			//imshow("Original", next);		
+			
+			Mat flow(prvs.size(), CV_32FC2);
+			calcOpticalFlowFarneback(prvs, next, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
+			//calcOpticalFlowFarneback(prvs, next, flow, 0.4, 1, 12, 2, 8, 1.2, 0);
+
+			// visualization
+			Mat flow_parts[2];
+			split(flow, flow_parts);
+			Mat magnitude, angle, magn_norm;
+			cartToPolar(flow_parts[0], flow_parts[1], magnitude, angle, true);
+			normalize(magnitude, magn_norm, 0.0f, 1.0f, NORM_MINMAX);
+			angle *= ((1.f / 360.f) * (180.f / 255.f));
+
+			//build hsv image
+			Mat _hsv[3], hsv, hsv8, bgr;
+			_hsv[0] = angle;
+			_hsv[1] = Mat::ones(angle.size(), CV_32F);
+			_hsv[2] = magn_norm;
+			merge(_hsv, 3, hsv);
+			hsv.convertTo(hsv8, CV_8U, 255.0);
+			cvtColor(hsv8, bgr, COLOR_HSV2BGR);
+			
+			warpPerspective(bgr,bgr,homograph,Size(frame2.size().width-400,frame2.size().height));
+			
+			bgr = bgr(rect);
+			
+			cvtColor(bgr, bgr, COLOR_BGR2GRAY);
+
+			//imshow("frame", bgr);
+			//waitKey(5);
+			
+			threshold( bgr, bgr, 5, 255, 3 );	
+
+			int totalPixels = bgr.rows*bgr.cols;
+			int countOn = countNonZero(bgr);
+			
+			double density = ((double)countOn)/((double)totalPixels);
+			
+			fout << count << "\t" << density << endl;
+
+			prvs = next;
+		
+		}
+		
+		count++;
+    
+    	}
+    	
+    	fout.close();
+    	
+    	VideoCapture cap("trafficvideo.mp4");
 
 	Mat frame,res1,res2,res,inver,outN,inverQ,emptyWarp;
 	
@@ -94,6 +187,7 @@ int main(int argc, char **argv)
 		
 		if(true)
 		{
+			
 			GaussianBlur(frame, frame, Size(3, 3), 0);
 			absdiff(frame,emptyImg,res1);
 			
@@ -106,7 +200,6 @@ int main(int argc, char **argv)
 
 			int totalPixels = cropN.rows*cropN.cols;
 			int countOn = countNonZero(cropN);
-			//int countOff = totalPixels-countOn;
 			
 			q_density = (double)countOn/totalPixels;
 			
