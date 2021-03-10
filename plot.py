464272a187193311a9d974111910feb0d24456fe
Aryan10333 <aryanchandra2@gmail.com>
2021-03-10 13:12:37 +0530

    Updated plot.py.

--- plot.py
+++ plot.py
@@ -5,16 +5,26 @@ g = open("queue_density.txt", "r")
 
 a = []
 b = []
+w = []
+z = []
 i = 0
 
 for k in g:
 	a = a + [float(k.split()[0])]
 	b = b + [i]
 	i =  i+1
+
+with open("dynamic_density.txt", "r") as file: 
+    data = file.readlines() 
+    for line in data: 
+        word = line.split() 
+        z.append(float(word[1]))
+        w.append(float(word[0]))
 	
 plt.figure(figsize=(9.6, 4.9))
 
 plt.plot(b,a)
+plt.plot(w, z)
 plt.xlabel('x - axis') 
 
 plt.ylabel('y - axis') 
